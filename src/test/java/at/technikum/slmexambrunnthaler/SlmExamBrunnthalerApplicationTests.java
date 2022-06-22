50b7606f0b4a7454a366ce49d72a158d8444482a
Herwig Brunnthaler <wi21b113@technikum-wien.at>
2022-06-22 19:49:37 +0200

    Initial commit

---
+++ src/test/java/at/technikum/slmexambrunnthaler/SlmExamBrunnthalerApplicationTests.java
@@ -0,0 +1,13 @@
+package at.technikum.slmexambrunnthaler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlmExamBrunnthalerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
